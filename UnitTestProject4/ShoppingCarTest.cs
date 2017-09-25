e608882acd5d28a2cf29a7c65603c3b19a0f7294
LukeLee <lukelee@nine-yi.com>
2017-09-26 07:29:29 +0800

    如果你一次買了整套一到五集，恭喜你將享有25%的折扣 --測試

--- UnitTestProject4/ShoppingCarTest.cs
+++ UnitTestProject4/ShoppingCarTest.cs
@@ -10,6 +10,7 @@ namespace UnitTestProject4
         [InlineData(2, 190)]
         [InlineData(3, 270)]
         [InlineData(4, 320)]
+        [InlineData(5, 375)]
         public void Test_GetResultl(int count, double expected)
         {
             //Araange
